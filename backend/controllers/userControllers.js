ce0b4a7f6c15563eb68e869eb29063fd23f18fb7
AliZia3 <alizia0217@gmail.com>
2023-08-09 21:22:27 -0400

    User search API Complete + implemented middleware to ensure current user does not show up in search

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,8 +1,10 @@
 // asyncHanlder automatically handles errors
-const asyncHandler = require("express-async-Handler");
+const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
 
+
+// Registration Controller
 const registerUser = asyncHandler(async (req, res) => {
 	// Extracting name, email, password, pic from the request body
 	const { name, email, password, pic } = req.body;
@@ -38,6 +40,7 @@ const registerUser = asyncHandler(async (req, res) => {
 	}
 });
 
+// Authentication Controller
 const authUser = asyncHandler(async (req, res) => {
 	const { email, password } = req.body;
 
@@ -57,4 +60,17 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 });
 
-module.exports = { registerUser, authUser };
+// User Search Controller
+const allUsers = asyncHandler(async (req, res) => {
+	const keyword = req.query.search ? {
+		$or: [
+			{ name: { $regex: req.query.search, $options: "i" }},
+			{ email: { $regex: req.query.search, $options: "i" }},
+		]
+	} : {};
+	// Want all users except the one that is logged in (current user)
+	const users = await User.find(keyword).find({_id:{$ne:req.user._id}})
+	res.send(users)
+});
+
+module.exports = { registerUser, authUser, allUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userControllers");
+const { registerUser, authUser, allUsers } = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 // when post request is made to /api/user/, it will be handled by the controller functions
-router.post("/", registerUser); // OR router.route('/').post(registerUser)
+router.route("/").post(registerUser).get(protect, allUsers);
 router.post("/login", authUser); // OR router.route("/login").post(authUser)
 
 module.exports = router;
