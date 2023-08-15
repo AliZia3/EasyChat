aa5a9074bb9dad4429987e1f3db57ae91f98df4d
AliZia3 <alizia0217@gmail.com>
2023-08-15 13:17:29 -0400

    Remove User from Group Chat API complete

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -138,7 +138,7 @@ const renameGroup = asyncHandler(async (req, res) => {
 const addToGroup = asyncHandler(async (req, res) => {
 	const { chatId, userId } = req.body;
 
-	const added = Chat.findByIdAndUpdate(
+	const added = await Chat.findByIdAndUpdate(
 		chatId,
 		{
 			$push: { users: userId },
@@ -159,7 +159,26 @@ const addToGroup = asyncHandler(async (req, res) => {
 });
 
 const removeFromGroup = asyncHandler(async (req, res) => {
-	return;
+	const { chatId, userId } = req.body;
+
+	const removed = await Chat.findByIdAndUpdate(
+		chatId,
+		{
+			$pull: { users: userId },
+		},
+		{
+			new: true,
+		}
+	)
+		.populate("users", "-password")
+		.populate("groupAdmin", "-password");
+
+	if (!removed) {
+		res.status(404);
+		throw new Error("Chat Not Found");
+	} else {
+		res.json(removed);
+	}
 });
 
 module.exports = {
